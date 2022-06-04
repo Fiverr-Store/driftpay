19e8b73ec8de39609623688662a7407b798d576e
ali-dev-code <ali.asgr514@gmail.com>
2022-06-04 14:54:51 +0500

    uploaded

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+(function () {
+	window.onload = function () {
+		window.setTimeout(fadeout, 0);
+	};
+  function fadeout() {
+		document.querySelector(".page-loader").style.opacity = "0";
+		document.querySelector(".page-loader").style.display = "none";
+	}
+	window.onscroll = function () {
+
+
+
+		var backToTo = document.querySelector(".scroll-top");
+		if (
+			document.body.scrollTop > 50 ||
+			document.documentElement.scrollTop > 50
+		) {
+			backToTo.style.display = "flex";
+		} else {
+			backToTo.style.display = "none";
+		}
+	};
+
+
+
+  	//===== mobile-menu-btn
+    let navbarToggler = document.querySelector(".navbar-toggler");
+    navbarToggler.addEventListener("click", function () {
+      navbarToggler.classList.toggle("active");
+    });
+
+
+
+
+})();
