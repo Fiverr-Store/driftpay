9792413440f6a3288f632556ca25115401a2f904
ali-dev-code <ali.asgr514@gmail.com>
2022-07-15 21:22:34 +0500

    done

--- mooncatz/assets/js/main.js
+++ mooncatz/assets/js/main.js
@@ -1,11 +1,5 @@
 (function () {
-	window.onload = function () {
-		window.setTimeout(fadeout, 500);
-	};
-  function fadeout() {
-		document.querySelector(".page-loader").style.opacity = "0";
-		document.querySelector(".page-loader").style.display = "none";
-	}
+
 	window.onscroll = function () {
 		var header_navbar = document.querySelector(".navbar-area");
 		var sticky = header_navbar.offsetTop;
