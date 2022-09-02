20ac3a2e342dffb8a8c53fa03ae5e81ae215a42d
ali-dev-code <ali.asgr514@gmail.com>
2022-09-02 22:31:38 +0500

    d

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,11 +23,6 @@
 
 
 
-  	//===== mobile-menu-btn
-    let navbarToggler = document.querySelector(".navbar-toggler");
-    navbarToggler.addEventListener("click", function () {
-      navbarToggler.classList.toggle("active");
-    });
 
 
 
